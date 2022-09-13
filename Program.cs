0297d6b6e8dc38ba49a7f4da3c85ff5d92aacc7b
arman sareskandi <42113160+itsarman82@users.noreply.github.com>
2022-09-13 14:12:07 +0430

    First release of the my first c# project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace intToBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple application to convert a string to a boolean
+
+            Console.WriteLine("enter the value of the boolean");
+
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput.GetType());
+
+            // get an input from user and get the value type
+
+            bool output = Convert.ToBoolean(userInput);
+            Console.WriteLine(output);
+            Console.WriteLine(output.GetType());
+
+            // convert the value to a boolean and return the value type
+        }
+    }
+}
